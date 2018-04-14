9f6bac8546999be67eff56f78625e40de3a0c03c
Shineeeee <msy960825@gmail.com>
2018-04-14 14:12:47 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Main.java
+ * 
+ * Main file
+ */
+
+/**
+ * Main class
+ * 
+ * @author SHINE
+ */
+public class Main {
+	/**
+	 * @param args Command line arguments
+	 * @author SHINE
+	 */
+	public static void main(String[] args) {
+		// TODO: will coding
+	}
+}
